f05b595a8d887908a6640f3ab957114b17acde2d
cocobongo <davce21_89@hotmail.com>
2025-07-18 17:09:01 -0500

    Make basic page layout
    - Update title to "OSM administrative divisions"
    - Remove unused vite.svg file and add globe.svg
    - Create Header component and add styles in Header.module.css
    - Remove unused rules index.css and define basic colors

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}><span>Administrative divisions</span><span>about</span></header>
+  )
+}
