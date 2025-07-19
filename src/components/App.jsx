2fcf6f2da16e2ecd0408ad015342ef70008b1dd8
diego vega <da-vce@hotmail.com>
2025-07-18 20:57:20 -0500

    Complete the basic page layout with header, main and footer
    - Create Main and Footer elements, and styles
    - Create header-footer height variable
    - Make Main use the remaining space
    - Change primary color variable
    - Use Sour Gummy font as default
    - Add basic components from fontawesome symbols
    - Add linkedin symbol to hyperlink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
-import Header from "./Header"
+import Header from "./Header.jsx"
+import Main from "./Main.jsx"
+import Footer from "./Footer.jsx"
 
 function App() {
   return (
-    <Header/>
-    
+    <>
+      <Header />
+      <Main />
+      <Footer/>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import styles from '../styles/Header.module.css'
 
 export default function Header() {
   return (
-    <header className={styles.header}><span>Administrative divisions</span><span>about</span></header>
+    <header className={styles.header}>
+      <span>Administrative divisions</span><span>about</span>
+      </header>
   )
 }
