18eb6e554e206aa67550f7f1a4d2206035e3959b
cocobongo <davce21_89@hotmail.com>
2025-07-19 17:18:56 -0500

    Improve page layout of side bar and main content
    - Move Footer from App.jsx to Main.jsx
    - Add main-body section that contains content a footer
    - Make the sidebar be sticky with width of 100vh
    - Make main-content section that will contain the actual data
    - Add a temporary popup data to show behavior
    
    This will make the aside selection always on screen
    The content data will be scroll-able

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import styles from '../styles/Main.module.css'
+import Footer from './Footer.jsx'
 
 export default function Main() {
+  const [show, setShow] = useState(true)
+  const [show2, setShow2] = useState(true)
   return (
     <main className={styles.main}>
-      <aside className={styles.aside}></aside>
-      <section id='main-content' className={styles['main-content']}></section>
+      <aside className={styles.aside}>
+        <button onClick={() => setShow(!show)}>expand</button>
+        <ul style={{ display: `${show ? 'block' : 'none'}` }}>
+          {Array(100).fill().map((_, i) => <li key={i} style={{ backgroundColor: 'red', margin: '1px' }}></li>)}
+        </ul>
+      </aside>
+      <section  className={styles['main-body']}>
+        <div className={styles['main-content']}>
+          <button onClick={() => setShow2(!show2)}>expand</button>
+          <div style={{ display: `${show2 ? 'block' : 'none'}`, height: '1000px', backgroundColor: 'red', margin: '10px' }}></div>
+
+        </div>
+        <Footer />
+      </section>
     </main>
   )
 }
