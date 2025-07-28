7260e00cab454072df8a2fa0e5761b282e20d2c9
cocobongo <davce21_89@hotmail.com>
2025-07-27 20:09:57 -0500

    feat: Add SearchDropdown component and integrate with Main component
    - Added SearchDropdown component for enhanced search functionality.
    - Integrated SearchDropdown into Main component, replacing the previous list display.
    - Updated package.json to include @emotion/react, @emotion/styled, @mui/material, and @mui/icons-material dependencies.
    - Modified App component to remove commented Footer component.
    - Adjusted Main.module.css to set a minimum width for the aside section.
    - Updated index.css to change the secondary color variable.
    - Created styles for SearchDropdown in a new SearchDropdown module CSS file.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import styles from '../styles/Main.module.css'
 import Footer from './Footer.jsx'
+import SearchDropdown from './SearchDropdown.jsx'
 
 export default function Main() {
   const [show, setShow] = useState(true)
-  const [show2, setShow2] = useState(true)
+  const [show2, setShow2] = useState(true);
+  const itemList = <ul style={{ display: `${show ? 'block' : 'none'}` }}>
+          {Array(100).fill().map((_, i) => <li key={i} style={{ backgroundColor: 'red', margin: '1px' }}></li>)}
+        </ul>;
   return (
     <main className={styles.main}>
       <aside className={styles.aside}>
-        <button onClick={() => setShow(!show)}>expand</button>
-        <ul style={{ display: `${show ? 'block' : 'none'}` }}>
-          {Array(100).fill().map((_, i) => <li key={i} style={{ backgroundColor: 'red', margin: '1px' }}></li>)}
-        </ul>
+        <SearchDropdown text='Search' content={itemList}/>
       </aside>
-      <section  className={styles['main-body']}>
+      <section className={styles['main-body']}>
         <div className={styles['main-content']}>
           <button onClick={() => setShow2(!show2)}>expand</button>
           <div style={{ display: `${show2 ? 'block' : 'none'}`, height: '1000px', backgroundColor: 'red', margin: '10px' }}></div>
---
+++ src/components/SearchDropdown.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/SearchDropdown.module.css'
+import { dropdown, dropdownIcon, searchFieldBox, searchField, searchFieldIconBox } from '../styles/SearchDropdown';
+import { useState } from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function SearchDropdown({ text = '', content }) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(prev => !prev);
+  }
+
+  const expandLess = <FontAwesomeIcon style={dropdownIcon(isOpen)} icon={faChevronRight} />;
+
+  return (
+    <Box>
+      <ListItemButton disableRipple sx={dropdown} onClick={handleClick}>
+        {expandLess}
+        <ListItemText className={styles.dropdownText} primary="Search" />
+      </ListItemButton>
+      <Collapse in={isOpen}>
+        <Box sx={searchFieldBox}>
+          <TextField
+            sx={searchField} placeholder="search" variant="outlined"
+          />
+          <Box sx={searchFieldIconBox}>
+            <FontAwesomeIcon icon={faSearch} />
+          </Box>
+        </Box>
+      </Collapse>
+    </Box>
+  )
+}
