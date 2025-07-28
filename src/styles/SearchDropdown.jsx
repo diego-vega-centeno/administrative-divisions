a09172239bff2cbbe254691a1a6977168939c9c5
diego vega <da-vce@hotmail.com>
2025-07-27 20:09:57 -0500

    feat: Add SearchDropdown component and integrate with Main component
    - Added SearchDropdown component for enhanced search functionality.
    - Integrated SearchDropdown into Main component, replacing the previous list display.
    - Updated package.json to include @emotion/react, @emotion/styled, @mui/material, and @mui/icons-material dependencies.
    - Modified App component to remove commented Footer component.
    - Adjusted Main.module.css to set a minimum width for the aside section.
    - Updated index.css to change the secondary color variable.
    - Created styles for SearchDropdown in a new SearchDropdown module CSS file.

---
+++ src/styles/SearchDropdown.jsx
@@ -0,0 +1,56 @@
+import TouchRipple from "@mui/material/ButtonBase/TouchRipple"
+import { alignItems, borderRadius, display, fontSize, height, justifyContent, margin, padding, width } from "@mui/system";
+
+const dropdown = {
+  backgroundColor: '#F8F9FA',
+  height: '2.3rem',
+  paddingLeft: '0.8rem',
+  '&:hover': {
+    backgroundColor: '#D3D4D5'
+  },
+  '&:active': {
+    backgroundColor: '#C6C7C8'
+  }
+}
+
+const dropdownIcon = (isOpen) => ({
+  width: '0.7rem',
+  fontSize: '1rem',
+  paddingRight: '0.5rem',
+  transform: isOpen ? 'rotate(90deg)' : 'rotate(0deg)',
+  transformOrigin: '25% 50%',
+  transition: 'transform ease 0.2s'
+});
+
+const searchFieldBox = {
+  display: 'flex',
+  margin: '0.4rem 1rem',
+  height: '2.5rem'
+}
+
+const searchField = {
+  backgroundColor: 'white',
+  '& .MuiInputBase-input': {
+    padding: '.25rem .5rem',
+    fontSize: '.9rem',
+    height: '100%'
+  },
+  '& .MuiInputBase-root': {
+    height: '100%'
+  }
+}
+
+const searchFieldIconBox = {
+  border: '1px solid grey',
+  borderRadius: '5px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '2.5rem',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: '#6C757D',
+  }
+}
+
+export { dropdown, dropdownIcon, searchFieldBox, searchField, searchFieldIconBox }
