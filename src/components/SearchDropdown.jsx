8510b929a33e93ec36b587f5dac22b563982ac25
cocobongo <davce21_89@hotmail.com>
2025-07-31 14:04:44 -0500

    Refactor SearchDropdown to use search and content props.
    - Define content in main and pass as props
    - Remove unused show variable
    - Move import dependencies to main
    - Clamp the width of the aside element

--- src/components/SearchDropdown.jsx
+++ src/components/SearchDropdown.jsx
@@ -1,13 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { dropdown, dropdownIcon, searchFieldBox, searchField, searchFieldIconBox } from '../styles/SearchDropdown';
+import { dropdown, dropdownIcon } from '../styles/SearchDropdown';
 import { useState } from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 
 export default function SearchDropdown({ text = '', content }) {
 
@@ -23,17 +21,10 @@ export default function SearchDropdown({ text = '', content }) {
     <Box>
       <ListItemButton disableRipple sx={dropdown} onClick={handleClick}>
         {expandLess}
-        <ListItemText primary="Search" />
+        <ListItemText primary={text} />
       </ListItemButton>
       <Collapse in={isOpen}>
-        <Box sx={searchFieldBox}>
-          <TextField
-            sx={searchField} placeholder="search" variant="outlined"
-          />
-          <Box sx={searchFieldIconBox}>
-            <FontAwesomeIcon icon={faSearch} />
-          </Box>
-        </Box>
+        {content}
       </Collapse>
     </Box>
   )
