568d6cfd3b298d8280ae0eac13f85570d4f4975f
cocobongo <davce21_89@hotmail.com>
2025-08-17 14:18:31 -0500

    Add basic nonatim osm search functionality

---
+++ src/utils/nominatim.js
@@ -0,0 +1,28 @@
+async function getNominatimSearch(query) {
+
+  const endpoint = 'https://nominatim.openstreetmap.org/search';
+
+  // display busy icon
+  // document.getElementById("entitySelectorBusyIcon").style.display = "block";
+  
+  // use the free-form query
+  const url = `${endpoint}?q=${query}&format=jsonv2`;
+  const res = await fetch(url, {
+    headers: { 'Accept': 'application/json' }
+  });
+
+  if (!res.ok) {
+    // display error message here
+  }
+
+  const entitiesFound = await res.json();
+  // filters to get relations only
+  const relationsFound = entitiesFound.filter(ele => ele.osm_type == "relation");
+
+  /* hide busy icon */
+  // document.getElementById("entitySelectorBusyIcon").style.display = "none";
+
+  return entitiesFound;
+}
+
+export { getNominatimSearch }
