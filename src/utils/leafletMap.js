25fd4ef121d0f53a2e08fb8f226e048c4de6b320
cocobongo <davce21_89@hotmail.com>
2025-08-22 15:06:29 -0500

    - Create layer and add to leaflet map - Show/hide progress icon

--- src/utils/leafletMap.js
+++ src/utils/leafletMap.js
@@ -1,12 +1,33 @@
 import osmtogeojson from 'osmtogeojson';
 
-function addToLeafletMap(osmBaseData, map) {
+async function addToLeafletMap(osmBaseData, map) {
   // make a deep copy of data
   const osmData = JSON.parse(JSON.stringify(osmBaseData));
 
-  const geojsonData = osmtogeojson(osmData);
-  console.log(geojsonData);
-  console.log(map);
+  // create layer from geojson data
+  const geojsonLayer = L.geoJSON(osmtogeojson(osmData), {
+    filter: function (feature, layer) {
+      return !(feature.id.includes('node'));
+    }
+  });
+
+  // remove previous layers
+  map.eachLayer(function (layer) {
+    map.removeLayer(layer);
+  });
+
+  // fit bounds to new layer
+  map.fitBounds(geojsonLayer.getBounds());
+
+  // populate with tiles
+  const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+
+  // add all layers
+  layer.addTo(map);
+  geojsonLayer.addTo(map);
 }
 
 export { addToLeafletMap }
