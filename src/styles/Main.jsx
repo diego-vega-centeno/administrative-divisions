9cbd7096a3d08b76dfa98ed68b318d8cd5e4e81f
cocobongo <davce21_89@hotmail.com>
2025-08-22 14:55:56 -0500

    Add progress icon when slippy map is loading

---
+++ src/styles/Main.jsx
@@ -0,0 +1,14 @@
+import { fontSize } from "@mui/system"
+
+const progressMapIcon = {
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '0.7rem 0',
+  '& .MuiCircularProgress-root': {
+    color: '#3F7251',
+  },
+  position: 'absolute',
+  zIndex: '1000'
+}
+
+export { progressMapIcon }
