005a53e6937cec60ed03003f2228427a85d6a5c1
cocobongo <davce21_89@hotmail.com>
2025-08-28 13:20:13 -0500

    Implement SubTable component to join other languages variations
    - Define SubTable component with own useState
    - Add styles in OSMTagsDropDown.module.css
    - Memoize the filtering and join of tags.
     - This is synchronous so don't use useEffect
     - Is kinda expensive so don't add it to the top
    - Use memoized filteredTags directly in osm tags table

--- src/components/OSMTagsDropDown.jsx
+++ src/components/OSMTagsDropDown.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { dropdownIcon } from '../styles/SearchDropdown.jsx';
 import { dropdown } from '../styles/OSMTagsDropDown.jsx';
 import styles from '../styles/OSMTagsDropDown.module.css'
@@ -19,6 +19,67 @@ export default function OSMTagsDropDown({ elementData }) {
     setIsOpen(prev => !prev);
   }
 
+  // SubTable compoenent needs to be in top level due to useState
+  function SubTable({ label, rows }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <div className={styles["sub-table-dropdown"]}>
+        <div className={styles["sub-table-toggle"]}>
+          {label} <span
+            onClick={() => setOpen(prev => !prev)}
+          >{"(more)"}</span>
+        </div>
+        <Collapse in={open}>
+          <table>
+            <tbody>
+              {rows.map(([key, value]) => (
+                <tr key={key} className={styles["row"]}>
+                  <th className={styles["cell-key"]}>{key}</th>
+                  <td className={styles["cell-value"]}>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Collapse>
+      </div>
+    )
+  }
+
+  // memoize the sub tables and recompute based on elemenData
+  const filteredTags = useMemo(() => {
+    const toFilterTags = ['name', 'official_name'];
+    const regexes = toFilterTags.map(tag => [tag, new RegExp(`^${tag}.+$`)]);
+    const matchedTags = Object.fromEntries(toFilterTags.map(tag => [tag, []]));
+    const filteredTags = {};
+
+    // iterate over element data tags
+    for (const [key, value] of Object.entries(elementData.tags)) {
+      const found = regexes.find(([tag, regex]) => regex.test(key));
+      // populate matches or filter tags
+      if (found) {
+        matchedTags[found[0]].push([key, value]);
+      } else {
+        filteredTags[key] = value;
+      }
+    }
+    console.log('matchedTags: ', matchedTags);
+
+    // Define SubTable and add to corresponding key
+    for (const [key, tags] of Object.entries(matchedTags)) {
+      if (tags.length > 0) {
+        filteredTags[key] = <SubTable
+          label={filteredTags[key]}
+          rows={tags}
+        />
+      }
+    }
+
+    return filteredTags
+  }, [elementData]);
+
+  console.log('filteredTags: ', filteredTags);
+
   return <Box>
     <ListItemButton sx={dropdown} disableRipple onClick={handleClick}>
       <FontAwesomeIcon style={dropdownIcon(isOpen)} icon={faChevronRight} />
@@ -30,7 +91,7 @@ export default function OSMTagsDropDown({ elementData }) {
           <h3 className={styles["header"]}>OSM tags</h3>
           <table className={styles["table"]}>
             <tbody>
-              {Object.entries(elementData.tags).map(([key, value]) => (
+              {Object.entries(filteredTags).map(([key, value]) => (
                 <tr key={key} className={styles["row"]}>
                   <th className={styles["cell-key"]}>{key}</th>
                   <td className={styles["cell-value"]}>{value}</td>
