beee9161bd8ffa0919de4a9db7280fbe8d4714b3
diego vega <da-vce@hotmail.com>
2026-01-12 16:38:33 -0500

    Implement navigation sidebar slide in
    - Use mui Drawer component with custom styling
    - Refactor Header.jsx to include nav side bar with better layout

---
+++ src/styles/navSidebar.jsx
@@ -0,0 +1,25 @@
+const navSideBox = {
+  backgroundColor: 'var(--color-secondary)',
+  height: '100%',
+  width: '10rem'
+}
+
+const navSideBarIcon = {
+  fontSize: '1.5rem',
+  padding: 2,
+  '&:hover': {
+    backgroundColor: 'red',
+  },
+  color: 'white'
+}
+
+const navSideBarButton = {
+  padding: '.4rem',
+  '&:hover': { backgroundColor: '#6C757D' }
+}
+
+const navSideItem = {
+  color: 'white'
+}
+
+export { navSideBox, navSideBarIcon, navSideItem, navSideBarButton }
