e13a19f140543318b40fddbb38d37621a91ac611
diego vega <da-vce@hotmail.com>
2026-01-12 16:38:33 -0500

    Implement navigation sidebar slide in
    - Use mui Drawer component with custom styling
    - Refactor Header.jsx to include nav side bar with better layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import styles from '../styles/Header.module.css'
+import NavSidebar from './navSidebar.jsx'
+import Box from '@mui/material/Box';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <span>Administrative divisions</span><span>about</span>
-      </header>
+      <span>Administrative divisions</span>
+      <div className={styles['header-menu']}>
+        <span>about</span>
+        <NavSidebar />
+      </div>
+    </header>
   )
 }
