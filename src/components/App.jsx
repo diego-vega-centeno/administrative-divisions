fb945377d17f909a1afb0d7a34dfe08fad7b770b
diego vega <da-vce@hotmail.com>
2026-01-15 16:39:16 -0500

    feat: Make updates to host app in vercel
    - Change from hash router to browser router
    - Remove base from vite config
    - Add vercel.json basic config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import Home from "../pages/Home.jsx"
-import { HashRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import OAuthCallback from "../pages/OAuthCallback.jsx"
 
 function App() {
-  return <HashRouter>
+  return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path={import.meta.env.VITE_AUTH_CALLBACK_URL} element={<OAuthCallback />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 }
 
 export default App
