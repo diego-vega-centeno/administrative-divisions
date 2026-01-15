a2663324b55097d1d8c9384cd134003c4b61be5f
diego vega <da-vce@hotmail.com>
2026-01-14 19:19:51 -0500

    feat: Implement OAuthCallback component to handle authentication response
    - Add Home.jsx and OAuthCallback.jsx under 'pages'
    - Remove unused 'base' config entry from vite
    - Define routes in App.jsx and use VITE_AUTH_CALLBACK_URL

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import Header from "./Header.jsx"
-import Main from "./Main.jsx"
-import Footer from "./Footer.jsx"
+import Home from "../pages/Home.jsx"
+import { BrowserRouter, Routes, Route } from "react-router"
+import OAuthCallback from "../pages/OAuthCallback.jsx"
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path={import.meta.env.VITE_AUTH_CALLBACK_URL} element={<OAuthCallback/>}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
