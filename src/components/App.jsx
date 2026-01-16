05cae01274fd59349c8cc8b09dc56d4c0026d852
cocobongo <da-vce@hotmail.com>
2026-01-16 18:09:18 -0500

    fix: Remove OAuthCallback router
    - Comment router because backend redirects to root with search parameters
    - Comment test of favorites endpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
 import Home from "../pages/Home.jsx"
 import { BrowserRouter, Routes, Route } from "react-router"
 import OAuthCallback from "../pages/OAuthCallback.jsx"
-import {useEffect} from "react"
+import { useEffect } from "react"
 import { errorLog } from "../utils/logger.js"
 
 function App() {
   // Wake up backend
   useEffect(() => {
+    // test favorites get
+    // fetch('http://localhost:3000/api/favorites', {
+    //   method: "GET",
+    //   credentials: "include"
+    // }).then(response => response.json())
+    //   .then(data => console.log('favorites: ', data))
+    //   .catch(error => {
+    //     console.log('error: ', error)
+    //   });
+
     if (import.meta.env.VITE_BACKEND_URL) {
       fetch(import.meta.env.VITE_BACKEND_URL + '/health', {
         method: "GET",
         cache: "no-cache",
-      }).catch((error) => {errorLog(`Could not wake up server: ${error}`)});
+      }).catch((error) => { errorLog(`Could not wake up server: ${error}`) });
     }
 
   }, []);
@@ -19,7 +29,7 @@ function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path={import.meta.env.VITE_AUTH_CALLBACK_URL} element={<OAuthCallback />} />
+      {/* <Route path={import.meta.env.VITE_AUTH_CALLBACK_URL} element={<OAuthCallback />} /> */}
     </Routes>
   </BrowserRouter>
 }
