fcf7dd615728b2d8c79b52eff1954f644002fb42
cocobongo <da-vce@hotmail.com>
2026-01-15 19:45:45 -0500

    fix: Add error log in case wake up fails

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ import Home from "../pages/Home.jsx"
 import { BrowserRouter, Routes, Route } from "react-router"
 import OAuthCallback from "../pages/OAuthCallback.jsx"
 import {useEffect} from "react"
+import { errorLog } from "../utils/logger.js"
 
 function App() {
   // Wake up backend
   useEffect(() => {
     if (import.meta.env.VITE_BACKEND_URL) {
-      fetch(import.meta.env.VITE_BACKEND_URL, {
+      fetch(import.meta.env.VITE_BACKEND_URL + '/health', {
         method: "GET",
         cache: "no-cache",
-      }).catch(() => { });
+      }).catch((error) => {errorLog(`Could not wake up server: ${error}`)});
     }
 
   }, []);
