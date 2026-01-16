5789c26b0b9bc19f47f0e4c65ab16149248cb35c
cocobongo <da-vce@hotmail.com>
2026-01-16 18:45:56 -0500

    feat: Implement alertDialog element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
 import Header from '../components/Header.jsx'
 import Main from '../components/Main.jsx'
+import { useSearchParams } from "react-router";
+import { useState, useEffect } from 'react';
+import { Dialog, Alert } from '@mui/material';
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+  const error = searchParams.get('error');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    if (error === 'oauth_failed') {
+      setErrorMessage('Authentication failed');
+    }
+  }, [error]);
+
+  const alertDialog = <>
+    <Dialog
+      open={Boolean(errorMessage)}
+      disableScrollLock
+    >
+      <Alert
+        severity="warning"
+        onClose={() => setErrorMessage(null)}
+      >
+        {errorMessage}
+      </Alert>
+    </Dialog>
+  </>
+
   return (
     <>
+      {alertDialog}
       <Header />
       <Main />
     </>
