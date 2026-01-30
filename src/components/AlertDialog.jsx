ae84c9588e35de7262e4141c8fe4602c03f8d823
diego vega <da-vce@hotmail.com>
2026-01-30 17:35:05 -0500

    fix: Remove disable scroll lock

--- src/components/AlertDialog.jsx
+++ src/components/AlertDialog.jsx
@@ -2,17 +2,12 @@ import { Dialog, Alert } from '@mui/material';
 
 export default function AlertDialog({ severity, message, open, onClose }) {
   return (
-    <Dialog
-      open={open}
-      disableScrollLock
-    >
+    <Dialog open={open}>
       <Alert
         severity={severity}
         onClose={onClose}
         sx={{ whiteSpace: 'pre-line' }}
-      >
-        {message}
-      </Alert>
+      >{message}</Alert>
     </Dialog>
   )
 }
