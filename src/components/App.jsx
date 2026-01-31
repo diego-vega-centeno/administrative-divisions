379bb75489ceebaec70d2abaaebbe77e0f981ca7
diego vega <da-vce@hotmail.com>
2026-01-31 13:30:49 -0500

    fix: Remove ping to wake up backend server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,26 +5,6 @@ import { useEffect } from "react"
 import { errorLog } from "../utils/logger.js"
 
 function App() {
-  // Wake up backend
-  useEffect(() => {
-    // test favorites get
-    // fetch('http://localhost:3000/api/favorites', {
-    //   method: "GET",
-    //   credentials: "include"
-    // }).then(response => response.json())
-    //   .then(data => console.log('favorites: ', data))
-    //   .catch(error => {
-    //     console.log('error: ', error)
-    //   });
-
-    if (import.meta.env.VITE_BACKEND_URL) {
-      fetch(import.meta.env.VITE_BACKEND_URL + '/health', {
-        method: "GET",
-        cache: "no-cache",
-      }).catch((error) => { errorLog(`Could not wake up server: ${error}`) });
-    }
-
-  }, []);
   return (
     <AuthProvider>
       <BrowserRouter>
