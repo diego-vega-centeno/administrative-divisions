5ac41a601434708bb2ef5c011001ac7e4c183d29
diego vega <da-vce@hotmail.com>
2026-02-01 23:02:12 -0500

    feat: improve alert style

--- src/components/AlertDialog.jsx
+++ src/components/AlertDialog.jsx
@@ -1,13 +1,32 @@
-import { Dialog, Alert } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import Alert from '@mui/material/Alert';
 
 export default function AlertDialog({ severity, message, open, onClose }) {
   return (
-    <Dialog open={open}>
+    <Dialog
+      open={open}
+      slotProps={{
+        paper: {
+          sx: {
+            position: 'fixed',
+            top: '10vh',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            boxShadow: 'none',
+            backgroundColor: 'transparent',
+          }
+        }
+      }}
+    >
       <Alert
         severity={severity}
         onClose={onClose}
-        sx={{ whiteSpace: 'pre-line' }}
+        sx={{
+          whiteSpace: 'pre-line',
+          backgroundColor: 'var(--color-primary)',
+          color: 'white'
+        }}
       >{message}</Alert>
-    </Dialog>
+    </Dialog >
   )
 }
