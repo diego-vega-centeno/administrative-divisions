b924fbc1bbbaa516dbc90f2c37726c35cc3bdacc
diego vega <da-vce@hotmail.com>
2026-02-05 16:32:33 -0500

    fix: delete layer button should disable edit mode

--- src/components/FavoritesMenuTable.jsx
+++ src/components/FavoritesMenuTable.jsx
@@ -136,6 +136,7 @@ export default function FavoritesMenuTable({
                         sx={headerCellToolsButton}
                         onClick={() => {
                           setConfirm(true);
+                          setEditMode(false);
                           setActiveLayer(layerId);
                         }}
                       >
