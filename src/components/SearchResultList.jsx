53aeb81f1659ae9e7b39cea9d83fe18ca6d196ea
diego vega <da-vce@hotmail.com>
2026-02-06 00:29:14 -0500

    fix: improve result item style

--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -2,11 +2,12 @@ import ListItem from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
 import { listItem, listBox } from '../styles/SearchResultList';
+import Typography from "@mui/material/Typography";
 
 export default function SearchResultList({ entities, onSelect }) {
 
   if (!entities || entities.length == 0) return null;
-  
+
   return (
     <Box sx={listBox}>
       {entities.map(ent => (
@@ -14,7 +15,10 @@ export default function SearchResultList({ entities, onSelect }) {
           sx={listItem} key={ent['osm_id']}
           onClick={() => onSelect(ent)}
         >
-          <ListItemText>{`${ent['display_name']} (${ent['addresstype']} - ${ent['osm_id']})`}</ListItemText>
+          <ListItemText>
+            <Typography component={'span'}>{`${ent['display_name']} `}</Typography>
+            <Typography component={'span'} color='black'>{`(${ent['addresstype']} - ${ent['osm_id']})`}</Typography>
+          </ListItemText>
         </ListItem>
       ))}
     </Box>
