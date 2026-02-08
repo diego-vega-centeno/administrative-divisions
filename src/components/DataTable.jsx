7780ad40b7f3e866302647dc9981b3be914f8dd7
diego vega <da-vce@hotmail.com>
2026-02-08 01:17:04 -0500

    feat: populate DataTable with props calculate from calculateFromGeo.js

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -10,6 +10,8 @@ import {
 } from "../styles/Menu.jsx";
 import { dataIndex, getParentNames } from "../utils/addData.js";
 import { useEffect, useState } from 'react';
+import { calculatePropsFromGeo } from '../utils/calculateFromGeo.js';
+import osmtogeojson from 'osmtogeojson';
 
 export default function DataTable({ osmRels }) {
   const [dataRels, setDataRels] = useState([])
@@ -17,14 +19,39 @@ export default function DataTable({ osmRels }) {
   useEffect(() => {
     const newDataRels = [];
     for (const rel of osmRels) {
+      // rel props
       const id = rel.id.toString();
-      const newRel = {};
-      newRel['id'] = id;
-      newRel['admin_level'] = dataIndex[id].admin_level;
-      newRel['text'] = dataIndex[id].text;
-      newRel['parents'] = getParentNames(id);
-      newDataRels.push(newRel);
+      const relProps = {};
+      relProps['id'] = id;
+      relProps['admin_level'] = dataIndex[id].admin_level;
+      relProps['text'] = dataIndex[id].text;
+      relProps['parents'] = getParentNames(id);
+      relProps['population'] = rel.tags?.population ? rel.tags.population : '--'
+          '--'
+
+      // geo computed props
+      const geoJSON = osmtogeojson({ elements: [rel] });
+      const calcProps = calculatePropsFromGeo(geoJSON);
+
+
+      // derived props
+      const derivedProps = {
+        popDensity: rel.tags?.population ?
+          rel.tags?.population / calcProps.area :
+          '--'
+      }
+
+      const stringProps = { ...relProps, ...calcProps, ...derivedProps };
+      for (const [key, val] of Object.entries(stringProps)) {
+        if(typeof val === 'number') {
+          stringProps[key] = val.toFixed(2);
+        }
+        
+      }
+      newDataRels.push(stringProps);
     }
+
+    console.log(newDataRels);
     setDataRels(newDataRels);
   }, [osmRels])
 
@@ -35,18 +62,22 @@ export default function DataTable({ osmRels }) {
         <TableHead >
           <TableRow >
             <TableCell align="center" sx={headerCell}>admin level</TableCell>
-            <TableCell align="center" sx={headerCell}>id</TableCell>
             <TableCell align="center" sx={headerCell}>name</TableCell>
-            <TableCell align="center" sx={headerCell}>parents</TableCell>
+            <TableCell align="center" sx={headerCell}>area (km²)</TableCell>
+            <TableCell align="center" sx={headerCell}>perimeter (km)</TableCell>
+            <TableCell align="center" sx={headerCell}>population</TableCell>
+            <TableCell align="center" sx={headerCell}>pop density (person/km²)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {dataRels.map((rel) => (
             <TableRow key={rel.id}>
               <TableCell align="center" sx={tableCell}>{rel.admin_level}</TableCell>
-              <TableCell align="center" sx={tableCell}>{rel.id}</TableCell>
               <TableCell align="center" sx={tableCell}>{rel.text}</TableCell>
-              <TableCell align="center" sx={tableCell}>{rel.parents}</TableCell>
+              <TableCell align="center" sx={tableCell}>{rel.area}</TableCell>
+              <TableCell align="center" sx={tableCell}>{rel.length}</TableCell>
+              <TableCell align="center" sx={tableCell}>{rel.population}</TableCell>
+              <TableCell align="center" sx={tableCell}>{rel.popDensity}</TableCell>
             </TableRow>
           ))}
         </TableBody>
