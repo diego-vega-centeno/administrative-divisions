cfcbc3a6d8f633ce60207d82aadb6cb34d28554b
diego vega <da-vce@hotmail.com>
2026-02-08 16:51:00 -0500

    feat: use a simple list item

--- src/components/TagsTable.jsx
+++ src/components/TagsTable.jsx
@@ -1,17 +1,18 @@
-import ListItem from '@mui/material/ListItemButton';
+import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import OSMTagsDropDown from './OSMTagsDropDown.jsx';
-import { dropdown } from '../styles/OSMTagsDropDown.jsx';
+import { tableContainerHeader } from '../styles/Menu.jsx';
+import Box from '@mui/material/Box';
 
 export default function TagsTable({ osmRels }) {
   return (
-    <div>
-      <ListItem disableRipple sx={dropdown}>
+    <Box>
+      <ListItem sx={tableContainerHeader}>
         <ListItemText primary={"Selected divisions tags"} />
       </ListItem>
       {osmRels.map(
         elementData => <OSMTagsDropDown key={elementData.id} elementData={elementData} />
       )}
-    </div>
+    </Box>
   )
 }
