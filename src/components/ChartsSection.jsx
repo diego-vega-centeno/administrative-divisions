f1770e6377c62e6f3ff3d2a40524f9fc3f17f1de
diego vega <da-vce@hotmail.com>
2026-02-10 15:04:17 -0500

    feat: use isComputingIconActive to conditionally return the component

--- src/components/ChartsSection.jsx
+++ src/components/ChartsSection.jsx
@@ -5,48 +5,62 @@ import {
   tableContainerHeader
 } from "../styles/Menu.jsx";
 import { chartsContainer, chartContainer } from '../styles/Menu.jsx';
-
+import CircularProgress from '@mui/material/CircularProgress';
+import { progressIcon } from '../styles/Main.jsx';
 import BarChart from './charts/BarChart.jsx';
 
-export default function ChartsSection({ computedDataRels }) {
+export default function ChartsSection({ computedDataRels, isComputingIconActive }) {
+
+  if (!computedDataRels.length && !isComputingIconActive) return null;
 
   return (
-    <Box>
-      <ListItem sx={tableContainerHeader}>
-        <ListItemText primary={"Charts"} />
-      </ListItem>
-      <Box sx={chartsContainer}>
-        <Box sx={chartContainer}>
-          <BarChart
-            computedDataRels={computedDataRels}
-            config={{
-              prop: 'population',
-              title: 'population',
-              color: 'rgba(30, 136, 229, 0.8)'
-            }}
-          />
-        </Box>
-        <Box sx={chartContainer}>
-          <BarChart
-            computedDataRels={computedDataRels}
-            config={{
-              prop: 'area',
-              title: 'area',
-              color: 'rgba(233, 237, 22, 0.8)'
-            }}
-          />
-        </Box>
-        <Box sx={chartContainer}>
-          <BarChart
-            computedDataRels={computedDataRels}
-            config={{
-              prop: 'popDensity',
-              title: 'population density',
-              color: 'rgba(233, 237, 22, 0.8)'
-            }}
-          />
+    isComputingIconActive ? (
+      <Box >
+        <ListItem sx={tableContainerHeader}>
+          <ListItemText primary={"Compare table"} />
+        </ListItem>
+        <Box sx={progressIcon}>
+          <CircularProgress thickness={9} size={70} />
         </Box>
       </Box>
-    </Box>
+    ) : (
+      <Box>
+        <ListItem sx={tableContainerHeader}>
+          <ListItemText primary={"Charts"} />
+        </ListItem>
+        <Box sx={chartsContainer}>
+          <Box sx={chartContainer}>
+            <BarChart
+              computedDataRels={computedDataRels}
+              config={{
+                prop: 'population',
+                title: 'population',
+                color: 'rgba(30, 136, 229, 0.8)'
+              }}
+            />
+          </Box>
+          <Box sx={chartContainer}>
+            <BarChart
+              computedDataRels={computedDataRels}
+              config={{
+                prop: 'area',
+                title: 'area',
+                color: 'rgba(233, 237, 22, 0.8)'
+              }}
+            />
+          </Box>
+          <Box sx={chartContainer}>
+            <BarChart
+              computedDataRels={computedDataRels}
+              config={{
+                prop: 'popDensity',
+                title: 'population density',
+                color: 'rgba(233, 237, 22, 0.8)'
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    )
   )
 }
