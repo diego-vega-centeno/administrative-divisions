2af588a5d55b0dc073298af5a758d35a1478a54e
diego vega <da-vce@hotmail.com>
2026-02-10 14:01:14 -0500

    feat: use worker to offload computation from the main thread
    - compute osmtogeojson in a sinlgle batch

---
+++ src/utils/worker.js
@@ -0,0 +1,56 @@
+import { dataIndex } from "./addData";
+import { getParentNames } from "./addData";
+import osmtogeojson from "osmtogeojson";
+import { calculatePropsFromGeo } from "./calculateFromGeo";
+
+self.onmessage = (e) => {
+  const osmRels = e.data;
+
+  const newDataRels = addComputedProps(osmRels);
+
+  self.postMessage(newDataRels);
+};
+
+
+function addComputedProps(osmRels) {
+
+  // batch osmtogeojson conversion
+  const geoJSON = osmtogeojson({ elements: osmRels });
+
+  const featureMap = new Map();
+  geoJSON.features.forEach(f => {
+    featureMap.set(f.id.toString().replace('relation/', ''), f);
+  });
+
+  return osmRels.map(rel => {
+
+    // rel props
+    const id = rel.id.toString();
+    const relProps = {
+      id,
+      admin_level: dataIndex[id].admin_level,
+      name: dataIndex[id].text,
+      parents: getParentNames(id),
+      population: rel.tags?.population ? parseInt(rel.tags.population) : null
+    };
+
+    // geo computed props
+    const feature = featureMap.get(rel.id.toString());
+    const calcProps = calculatePropsFromGeo(feature);
+
+    // derived props
+    const derivedProps = {
+      popDensity: rel.tags?.population ?
+        rel.tags?.population / calcProps.area :
+        null
+    }
+
+    const allProps = { ...relProps, ...calcProps, ...derivedProps };
+
+    // simulate delay
+    // const start = Date.now();
+    // while (Date.now() - start < 2000) { }
+
+    return allProps;
+  })
+}
