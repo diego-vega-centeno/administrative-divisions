10e806b66eb7580082eb98dc72253f043cd13363
diego vega <da-vce@hotmail.com>
2026-02-12 10:41:35 -0500

    feat: use better formatting from ListItemText

--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -2,7 +2,6 @@ import ListItem from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
 import { listItem, listBox } from '../styles/SearchResultList';
-import Typography from "@mui/material/Typography";
 
 export default function SearchResultList({ entities, onSelect }) {
 
@@ -15,10 +14,16 @@ export default function SearchResultList({ entities, onSelect }) {
           sx={listItem} key={ent['osm_id']}
           onClick={() => onSelect(ent)}
         >
-          <ListItemText>
-            <Typography component={'span'}>{`${ent['display_name']} `}</Typography>
-            <Typography component={'span'} color='black'>{`(${ent['addresstype']} - ${ent['osm_id']})`}</Typography>
-          </ListItemText>
+          <ListItemText
+            primary={ent.display_name}
+            secondary={`${ent.addresstype} - ${ent.osm_id}`}
+            slotProps={{
+              secondary: {
+                fontSize: '0.9rem',
+                color: 'black'
+              }
+            }}
+          />
         </ListItem>
       ))}
     </Box>
