c8e8c32b33dd81bdbc38ea0f76381b145357cdc1
diego vega <da-vce@hotmail.com>
2026-03-15 22:13:56 -0500

    feat: add type declarations file
    - include css modules and env variables

---
+++ src/declarations.d.ts
@@ -0,0 +1,14 @@
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string;
+  readonly VITE_GOOGLE_AUTH_URL: string;
+  readonly VITE_OSM_AUTH_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
