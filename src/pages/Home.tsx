8cce140b31b946a0539009b431464322c244286b
diego vega <da-vce@hotmail.com>
2026-03-16 18:24:16 -0500

    feat: migrate Home and About

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import Header from '../components/Header.jsx'
-import Main from '../components/Main.jsx'
+import Header from '../components/Header'
+import Main from '../components/Main'
 
 export default function Home() {
 
