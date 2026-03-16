a6288ee7985cf9cf65489304b4e04873055a5a30
diego vega <da-vce@hotmail.com>
2026-03-15 21:32:20 -0500

    feat: base file to migrate to typescript
    - add types dependecies
    - migrate main.jsx and App.jsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App.jsx'
+import App from './components/App'
 import { cleanDBCache } from './utils/indexedDB.js'
 
 //  Clean cache, databse is initialized inside
 cleanDBCache();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
