5690e4af81c990f1cc024cabd0bba38380df203f
diego vega <da-vce@hotmail.com>
2026-03-16 17:56:01 -0500

    feat: migrate FavoritesMenuTable.tsx
    - introduce RelationsUserLayersType to src/types/index.ts
    - use RelationsUserLayersType in FavoritesMenuTable props

---
+++ src/components/FavoritesMenuTable.tsx
@@ -0,0 +1,278 @@
+import {
+  table,
+  tableCell,
+  headerCell,
+  subHeaderCell,
+  headerCellContent,
+  headerCellToolsContainer,
+  headerCellToolsButton,
+  headerCellConfirmContainer,
+  favoritesMenuCheckbox,
+  favoritesMenuCheckboxCell,
+  saveMenuEditText,
+} from "../styles/Menu.js";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareUpRight,
+  faTrash,
+  faX,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquare, faCheckSquare } from "@fortawesome/free-regular-svg-icons";
+// import { dataIndex, getParentNames } from "../utils/addData.js";
+import { addToolsButton } from "../styles/SelectAddDropdown";
+import TextField from "@mui/material/TextField";
+import { Dispatch, SetStateAction, useState } from "react";
+import { RelationsType, RelationsUserLayersType } from "../types/index.ts";
+
+interface FavoritesMenuTableProps {
+  activeLayer: string;
+  layerRels: RelationsUserLayersType[];
+  setActiveLayer: Dispatch<SetStateAction<string>>;
+  plotLayer: (_: string) => void;
+  groupKey: string;
+  deleteSelectedLayer: (_: string) => void;
+  editMode: boolean;
+  setEditMode: Dispatch<SetStateAction<boolean>>;
+  confirm: boolean;
+  setConfirm: Dispatch<SetStateAction<boolean>>;
+  selectedLayerRelsIds: Set<string>;
+  setSelectedLayerRelsIds: Dispatch<SetStateAction<Set<string>>>;
+  error: string;
+  setError: Dispatch<SetStateAction<string>>;
+  handleEditSave: (
+    groupKey: string,
+    layerId: string,
+    layerTitle: string,
+    newTitle: string,
+  ) => void;
+}
+
+export default function FavoritesMenuTable({
+  activeLayer,
+  layerRels,
+  setActiveLayer,
+  plotLayer,
+  groupKey,
+  deleteSelectedLayer,
+  editMode,
+  setEditMode,
+  confirm,
+  setConfirm,
+  selectedLayerRelsIds,
+  setSelectedLayerRelsIds,
+  error,
+  setError,
+  handleEditSave,
+}: FavoritesMenuTableProps) {
+  const [layerTitle, layerId] = groupKey.split("|");
+  const [newTitle, setNewTitle] = useState(layerTitle);
+
+  const handleDeleteLayer = (layerId: string) => {
+    // send delete request to parent
+    deleteSelectedLayer(layerId);
+    // disable comfirm buttons
+    setConfirm(false);
+  };
+
+  const handlePlotLayer = (groupKey: string) => {
+    plotLayer(groupKey);
+  };
+
+  const toggle = (id: string) => {
+    setSelectedLayerRelsIds((prev: Set<string>) => {
+      const newSelected = new Set(prev);
+      newSelected.has(id) ? newSelected.delete(id) : newSelected.add(id);
+      return newSelected;
+    });
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+    setSelectedLayerRelsIds(new Set());
+    setConfirm(false);
+    setNewTitle(layerTitle);
+    setError("");
+  };
+
+  return (
+    <Table sx={table}>
+      <TableHead>
+        <TableRow>
+          <TableCell align="center" sx={headerCell} colSpan={5}>
+            <Box sx={headerCellContent}>
+              {editMode && !confirm && activeLayer == layerId ? (
+                <TextField
+                  value={newTitle}
+                  onChange={(e) => {
+                    setNewTitle(e.target.value);
+                    if (error) setError("");
+                  }}
+                  sx={saveMenuEditText}
+                  error={Boolean(error)}
+                  helperText={error}
+                />
+              ) : (
+                <Typography>{layerTitle}</Typography>
+              )}
+              {confirm && !editMode && activeLayer === layerId ? (
+                <Box
+                  sx={headerCellConfirmContainer}
+                  className="header-cell-tools"
+                >
+                  <Button
+                    sx={{ ...headerCellToolsButton, color: "rgb(218 9 9)" }}
+                    onClick={() => handleDeleteLayer(layerId)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </Button>
+                  <Button
+                    sx={headerCellToolsButton}
+                    onClick={() => setConfirm(false)}
+                  >
+                    <FontAwesomeIcon icon={faX} />
+                  </Button>
+                </Box>
+              ) : editMode && !confirm && activeLayer == layerId ? (
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Box>
+                    <Box component={"span"}>
+                      {selectedLayerRelsIds.size} relations will be deleted
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Button
+                      sx={addToolsButton}
+                      size="small"
+                      variant="contained"
+                      onClick={() =>
+                        handleEditSave(groupKey, layerId, layerTitle, newTitle)
+                      }
+                    >
+                      save
+                    </Button>
+                    <Button
+                      sx={addToolsButton}
+                      size="small"
+                      variant="contained"
+                      onClick={() => handleCancel()}
+                    >
+                      cancel
+                    </Button>
+                  </Box>
+                </Box>
+              ) : (
+                <Box
+                  sx={headerCellToolsContainer}
+                  className="header-cell-tools"
+                >
+                  <Tooltip title="Edit" placement="top" arrow>
+                    <Button
+                      sx={headerCellToolsButton}
+                      onClick={() => {
+                        setEditMode(true);
+                        setConfirm(false);
+                        setActiveLayer(layerId);
+                        setSelectedLayerRelsIds(new Set());
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip title="Plot" placement="top" arrow>
+                    <Button
+                      sx={headerCellToolsButton}
+                      onClick={() => handlePlotLayer(groupKey)}
+                    >
+                      <FontAwesomeIcon icon={faSquareUpRight} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip title="Delete" placement="top" arrow>
+                    <Button
+                      sx={headerCellToolsButton}
+                      onClick={() => {
+                        setConfirm(true);
+                        setEditMode(false);
+                        setActiveLayer(layerId);
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                  </Tooltip>
+                </Box>
+              )}
+            </Box>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell align="center" sx={subHeaderCell}></TableCell>
+          <TableCell align="center" sx={subHeaderCell}>
+            admin level
+          </TableCell>
+          <TableCell align="center" sx={subHeaderCell}>
+            id
+          </TableCell>
+          <TableCell align="center" sx={subHeaderCell}>
+            name
+          </TableCell>
+          <TableCell align="center" sx={subHeaderCell}>
+            parents names
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {layerRels.map((rel) => {
+          return (
+            <TableRow key={rel.id}>
+              <TableCell align="center" sx={favoritesMenuCheckboxCell}>
+                {editMode && activeLayer === layerId && (
+                  <Button
+                    size="small"
+                    sx={favoritesMenuCheckbox}
+                    onClick={() => toggle(rel.id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={
+                        selectedLayerRelsIds.has(rel.id) ? faTrash : faSquare
+                      }
+                      style={{
+                        color: selectedLayerRelsIds.has(rel.id) ? "red" : "",
+                      }}
+                    />
+                  </Button>
+                )}
+              </TableCell>
+              <TableCell align="center" sx={tableCell}>
+                {rel.admin_level}
+              </TableCell>
+              <TableCell align="center" sx={tableCell}>
+                {rel.osm_relation_id}
+              </TableCell>
+              <TableCell align="center" sx={tableCell}>
+                {rel.osm_relation_name}
+              </TableCell>
+              <TableCell align="center" sx={tableCell}>
+                {rel.parents_names}
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,17 @@ export interface RelationsType {
   id: string;
   layer_id: string;
   layer_title: string;
+  admin_level: string;
+}
+
+export interface RelationsUserLayersType {
+  id: string;
+  layer_id: string;
+  layer_title: string;
   osm_relation_id: string;
   admin_level: string;
+  osm_relation_name: string;
+  parents_names: string;
 }
 
 export interface CustomError extends Error {
