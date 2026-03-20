c6969b16e152b2ff73a8be632c67fc9da6f932b1
diego vega <da-vce@hotmail.com>
2026-03-20 18:04:15 -0500

    feat: add null type to return value

--- src/utils/calculateFromGeo.ts
+++ src/utils/calculateFromGeo.ts
@@ -2,8 +2,10 @@ import { area } from "@turf/area";
 import { length } from "@turf/length";
 import { Feature, GeoJsonProperties, Geometry } from "geojson";
 
-function calculatePropsFromGeo(geo:Feature<Geometry, GeoJsonProperties>
-) {
+function calculatePropsFromGeo(geo: Feature<Geometry, GeoJsonProperties>): {
+  area: number | null;
+  perimeter: number | null;
+} {
   return {
     area: area(geo) / 1_000_000, // (km)^2
     perimeter: length(geo), // km,
